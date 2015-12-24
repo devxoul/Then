38de008d32ef40cdb3569cd2f42e1a258073ed8f
Suyeol Jeon <devxoul@gmail.com>
2015-12-24 17:35:35 +0900

    Start Xcode project

---
+++ ThenTests/ThenTests.swift
@@ -0,0 +1,36 @@
+//
+//  ThenTests.swift
+//  ThenTests
+//
+//  Created by 전수열 on 12/24/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+import XCTest
+@testable import Then
+
+class ThenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
