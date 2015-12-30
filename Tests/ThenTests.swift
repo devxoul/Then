914c66bb351008145bf49faf08d44ae3e0016056
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2015-12-30 08:30:24 -0800

    Add Swift Package Manager support

---
+++ Tests/ThenTests.swift
@@ -0,0 +1,23 @@
+//
+//  ThenTests.swift
+//  ThenTests
+//
+//  Created by 전수열 on 12/24/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+import XCTest
+@testable import Then
+
+class ThenTests: XCTestCase {
+
+    func testThen() {
+        let label = UILabel().then {
+            $0.text = "I am a label."
+            $0.textColor = .blackColor()
+        }
+        XCTAssertEqual(label.text, "I am a label.")
+        XCTAssertEqual(label.textColor, UIColor.blackColor())
+    }
+    
+}
