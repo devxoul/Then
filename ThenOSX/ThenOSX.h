b9ddf0100aaca7521e94824e318c803fda327450
phimage <ssjgotrunk@yahoo.fr>
2016-01-05 17:42:02 +0100

    Add TVOS & OSX target in project and cocoapod podspec

---
+++ ThenOSX/ThenOSX.h
@@ -0,0 +1,19 @@
+//
+//  ThenOSX.h
+//  ThenOSX
+//
+//  Created by phimage on 05/01/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ThenOSX.
+FOUNDATION_EXPORT double ThenOSXVersionNumber;
+
+//! Project version string for ThenOSX.
+FOUNDATION_EXPORT const unsigned char ThenOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThenOSX/PublicHeader.h>
+
+
