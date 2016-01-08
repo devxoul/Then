c138803e7f38cb0dcae1f9e675cbd6b2938deaf4
Jiří Třečák <jiritrecak@gmail.com>
2016-01-08 16:01:21 +0100

    Added "when", which will only execute when initial condition is met

--- ThenTests/ThenTests.swift
+++ ThenTests/ThenTests.swift
@@ -12,12 +12,37 @@ import XCTest
 class ThenTests: XCTestCase {
 
     func testThen() {
+        // Configure default UILabel
         let label = UILabel().then {
             $0.text = "I am a label."
             $0.textColor = .blackColor()
         }
+        
+        // Test color and text equality
         XCTAssertEqual(label.text, "I am a label.")
         XCTAssertEqual(label.textColor, UIColor.blackColor())
     }
     
+    
+    func testWhen() {
+        // Configure default UILabel
+        let label = UILabel().when(true) {
+            $0.text = "I am a label."
+            $0.textColor = .blackColor()
+        }
+        
+        // Test color and text equality
+        XCTAssertEqual(label.text, "I am a label.")
+        XCTAssertEqual(label.textColor, UIColor.blackColor())
+        
+        // Try to reconfigure it
+        label.when(false) {
+            $0.text = "I will break your test if true."
+            $0.textColor = .whiteColor()
+        }
+        
+        // Test color and text equality again
+        XCTAssertEqual(label.text, "I am a label.")
+        XCTAssertEqual(label.textColor, UIColor.blackColor())
+    }
 }
