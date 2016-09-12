673a4d07bd6c825f9c9dbcda3a85f1c440e7b9dd
Suyeol Jeon <devxoul@gmail.com>
2016-09-12 16:48:53 +0900

    Use 2 spaces for indentation

--- Sources/Then.swift
+++ Sources/Then.swift
@@ -27,44 +27,44 @@ public protocol Then {}
 
 extension Then where Self: Any {
 
-    /// Makes it available to set properties with closures just after initializing.
-    ///
-    ///     let frame = CGRect().with {
-    ///         $0.origin.x = 10
-    ///         $0.size.width = 100
-    ///     }
-    public func with(_ block: (inout Self) -> Void) -> Self {
-        var copy = self
-        block(&copy)
-        return copy
-    }
+  /// Makes it available to set properties with closures just after initializing.
+  ///
+  ///     let frame = CGRect().with {
+  ///       $0.origin.x = 10
+  ///       $0.size.width = 100
+  ///     }
+  public func with(_ block: (inout Self) -> Void) -> Self {
+    var copy = self
+    block(&copy)
+    return copy
+  }
 
 }
 
 extension Then where Self: AnyObject {
 
-    /// Makes it available to set properties with closures just after initializing.
-    ///
-    ///     let label = UILabel().then {
-    ///         $0.textAlignment = .Center
-    ///         $0.textColor = UIColor.blackColor()
-    ///         $0.text = "Hello, World!"
-    ///     }
-    public func then(_ block: (Self) -> Void) -> Self {
-        block(self)
-        return self
-    }
+  /// Makes it available to set properties with closures just after initializing.
+  ///
+  ///     let label = UILabel().then {
+  ///       $0.textAlignment = .Center
+  ///       $0.textColor = UIColor.blackColor()
+  ///       $0.text = "Hello, World!"
+  ///     }
+  public func then(_ block: (Self) -> Void) -> Self {
+    block(self)
+    return self
+  }
 
-    /// Makes it available to execute something with closures.
-    ///
-    ///     UserDefaults.standard.do {
-    ///         $0.set("devxoul", forKey: "username")
-    ///         $0.set("devxoul@gmail.com", forKey: "email")
-    ///         $0.synchronize()
-    ///     }
-    public func `do`(_ block: (Self) -> Void) {
-        block(self)
-    }
+  /// Makes it available to execute something with closures.
+  ///
+  ///     UserDefaults.standard.do {
+  ///       $0.set("devxoul", forKey: "username")
+  ///       $0.set("devxoul@gmail.com", forKey: "email")
+  ///       $0.synchronize()
+  ///     }
+  public func `do`(_ block: (Self) -> Void) {
+    block(self)
+  }
 
 }
 
