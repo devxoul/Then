37494cd903b96c89ee24867dd8c09637f67e8c59
Shang <shang1219178163@gmail.com>
2021-06-21 16:24:09 +0800

    add  @discardableResult

--- Sources/Then/Then.swift
+++ Sources/Then/Then.swift
@@ -68,6 +68,7 @@ extension Then where Self: AnyObject {
   ///       $0.textColor = UIColor.black
   ///       $0.text = "Hello, World!"
   ///     }
+  @discardableResult
   @inlinable
   public func then(_ block: (Self) throws -> Void) rethrows -> Self {
     try block(self)
