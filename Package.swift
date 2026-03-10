31ffbdf375fd2c8ee1468c9afe93fa0f00ab4144
Jeon Suyeol <devxoul@gmail.com>
2026-03-11 08:37:52 +0900

    Fix Swift 6 MainActor isolation by bumping swift-tools-version to 6.0
    In Swift 6 language mode, non-escaping closures automatically inherit
    the caller's actor isolation. This resolves the type mismatch error when
    using then/with/do with @MainActor-isolated types such as UIKit classes.
    
    Closes #106

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
   name: "Then",
   platforms: [
-    .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2),
+    .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4),
   ],
   products: [
     .library(name: "Then", targets: ["Then"]),
